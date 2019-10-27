b4c31c04687b038419f9f1d26951bdd744bf9d0d
Benjamin Warner <benjaminwarner@u.boisestate.edu>
2019-10-27 11:33:45 -0600

    Implement the put method and probing mechanism.

--- src/HashTable.java
+++ src/HashTable.java
@@ -6,34 +6,48 @@ public class HashTable<V> {
 	private int capacity;
 	private float loadFactor;
 	private int size;
+	private int numProbes;
 	private int maxSize;
 
 	private final int DEFAULT_CAPACITY = 13;
-	private final float DEFAULT_LOAD_FACTOR = .75;
+	private final float DEFAULT_LOAD_FACTOR = (float).75;
 	private final OpenAddressType DEFAULT_TYPE = OpenAddressType.linear;
 
 	public HashTable() {
-		this(DEFAULT_CAPACITY, DEFAULT_LOAD_FACTOR, DEFAULT_TYPE);
+		setupFields(DEFAULT_CAPACITY, DEFAULT_LOAD_FACTOR, DEFAULT_TYPE);
 	}
 
 	public HashTable(int capacity) {
-		this(capacity, DEFAULT_LOAD_FACTOR, DEFAULT_TYPE);
+		setupFields(capacity, DEFAULT_LOAD_FACTOR, DEFAULT_TYPE);
 	}
 
 	public HashTable(int capacity, float loadFactor) {
-		this(capacity, loadFactor, DEFAULT_TYPE);
+		setupFields(capacity, loadFactor, DEFAULT_TYPE);
 	}
 
 	public HashTable(int capacity, float loadFactor, OpenAddressType type) {
+		setupFields(capacity, loadFactor, type);	
+	}
+
+	@SuppressWarnings("unchecked")
+	private void setupFields(int capacity, float loadFactor, OpenAddressType type) {
 		this.capacity = capacity;
 		this.loadFactor = loadFactor;
 		this.type = type;
-		this.hashTable = new HashObject<V>[];
-		this.frequency = new int[];
+		this.hashTable = new HashObject[this.capacity];
+		this.frequency = new int[this.capacity];
 		this.size = 0;
+		this.numProbes = 0;
+		this.maxSize = (int)(this.loadFactor * this.capacity);
 	}
 
 	public void put(V value, int key) {
+		if (this.size + 1 > this.maxSize)
+			throw new IllegalStateException();
+		HashObject<V> element = new HashObject<V>(value, key);
+		int index = this.probe(key);
+		this.hashTable[index] = element;
+		++this.size;
 	}
 
 	public V remove(V value, int key) {
@@ -44,19 +58,43 @@ public class HashTable<V> {
 		return false;
 	}
 
+	private int probe(int key) {
+		int index = 0;
+		HashObject<V> element = null;
+		for (this.numProbes = 0; this.numProbes < this.capacity; ++numProbes) {
+			index = getHash(key, this.numProbes);
+			element = this.hashTable[index];
+			if (element == null)
+				break;
+		}
+		if (element != null)
+			return -1;
+		return index;
+	}
+
 	public void clear() {
 	}
 
-	public int getHash(int something, int somethingElse) {
+	public int getHash(int key, int numProbes) {
+		switch (this.type) {
+			case linear:
+				return (hash1(key) + numProbes) % this.capacity;
+			case quadratic:
+				return -1;
+			case doubleHashing:
+				return (hash1(key) + numProbes * hash2(key)) % this.capacity;
+			default:
+				break;
+		}
 		return 0;
 	}
 
-	private int hash1(int h, int k) {
-		return 0;
+	private int hash1(int key) {
+		return key % this.capacity;
 	}
 
-	private int hash2(int h, int k) {
-		return 0;
+	private int hash2(int key) {
+		return 1 + (key % (this.capacity - 2));
 	}
 
 	public OpenAddressType getType() {
@@ -67,7 +105,7 @@ public class HashTable<V> {
 		return capacity;
 	}
 
-	public int getLoadFactor() {
+	public float getLoadFactor() {
 		return loadFactor;
 	}
 
@@ -96,6 +134,15 @@ public class HashTable<V> {
 	}
 
 	public String toString() {
-		return "";
+		String s = "[";
+		for (int i = 0; i < this.capacity - 1; ++i) {
+			if (this.hashTable[i] == null)
+				s += "N/A, ";
+			else
+				s += this.hashTable[i].toString() + ", ";
+		}
+		s += this.hashTable[this.capacity - 1] == null ? "N/A" : this.hashTable[this.capacity - 1].toString();
+		s += "]";
+		return s;
 	}
 }
--- src/HashTest.java
+++ src/HashTest.java
@@ -1,6 +1,13 @@
 public class HashTest {
     
     public static void main(String[] args) {
-		System.out.println("Hello world!");
+		HashTable<String> table = new HashTable<String>();
+		table.put("John", 2);
+		table.put("Sara", 13);
+		table.put("Mary", 22);
+		table.put("Sue", 1);
+		table.put("Bob", 2);
+		table.put("Joseph", 14);
+		System.out.println(table.toString());
     }
 }
